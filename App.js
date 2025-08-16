2c1d63fcc0bf7534da287f03088ef318d2e1dc65
dudagasparotto <dudagasparotto05@gmail.com>
2025-08-15 22:44:52 -0300

    Começando exemplo 01

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Exemplo01 from './src/exemplos';
+
 export default function App() {
   return (
     <View style={styles.container}>
