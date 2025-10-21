a99854d8b9036c069c6dde4bd0ad6a3f3b505015
dudagasparotto <dudagasparotto05@gmail.com>
2025-10-20 22:50:01 -0300

    atividade 6

--- src/exemplos/ex06/index.js
+++ src/exemplos/ex06/index.js
@@ -1,43 +1,72 @@
 import React, {useState} from 'react'; 
 import  {Text, View, StyleSheet, TextInput, TouchableOpacity} from 'react-native'; 
 
+import styles from './styles';
 
 export default function Exemplo06 (){
+
+    const [massa, setMassa] = useState(0);
+    const [altura, setAltura] = useState(0);
+    const [resultado, setResultado] = useState(0);
+
+    function Calcular(){
+        const valor = massa / (altura * altura);
+        setResultado(valor);
+
+    if (valor < 18.5 ){
+        console.log('Abaixo do peso');
+    } 
+    if (valor >= 18.5 && valor < 24.9){
+        console.log('Peso ideal');
+    }
+    if (valor >= 25 && vaor < 29.9){
+        console.log('Sobrepeso');
+    }
+    if (valor >= 30 && valor < 34.9){
+        console.log('Obesidade grau 1');
+    }
+    if (valor >= 35 && valor < 39.9){
+        console.log('Obesidade grau 2 (severa)');
+    }
+    if (valor >= 40){
+        console.log('Obesidade grau 3 (mórbida)');
+    }
+
+    } 
+
+
     return(
         <View style={styles.container}>
             <Text style={styles.paragraph}>Exemplo 6</Text>
+
             <View style={styles.entradaImc}>
-            <TextInput 
-            placeholder='Massa' 
-            placeholderTextColor='lightgray' 
-            keyboardType='numeric' 
-            style={styles.input} />
-        <TextInput 
-        placeholder='Altura' 
-        placeholderTextColor='lightgray' 
-        keyboardType='numeric' 
-        style={styles.input} />
-  </View>
-</View>
+                <TextInput 
+                placeholder='Massa' 
+                placeholderTextColor='lightgray' 
+                keyboardType='numeric' 
+                style={styles.input}
+                onChangeText={(entrada) => setMassa (entrada)} />
+
+                <TextInput 
+                placeholder='Altura' 
+                placeholderTextColor='lightgray' 
+                keyboardType='numeric' 
+                style={styles.input} 
+                onChangeText={(entrada) => setAltura (entrada)}/> 
+            </View>
+
+            <TouchableOpacity style={styles.botao} onPress={() => Calcular ()} >
+            <Text style={styles.botaoText}> Calcular IMC </Text>
+            </TouchableOpacity>
+
+            <Text style={styles.resultados}>{resultado.toFixed(2)}</Text>
+
+           
+
+        </View>
+        
+        
 
     );
 }
-  const styles = StyleSheet.create({
-    container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 8,
-    },
-    paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#34495e',
-    },
-    entradaIMC:{
-        flex: 1,
-        flexDirection: 'row',
-    },
-});
+
--- src/exemplos/ex06/styles.js
+++ src/exemplos/ex06/styles.js
@@ -3,28 +3,57 @@ import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 
 const styles = StyleSheet.create(
     {
-        container:{
-            flex: 1,
-            backgroundColor: '#ff0',
-            alignItems: 'center',
-            justifyContent: 'space-evenly',
-            padding: 8,
-        },
-        titulo:{
-            fintSize: RFValue(5),
-            color:'#777',
-            fontWeight: 'bold',
-            borderColor: 'deeppink',
-            borderWidth: 2,
-            padding: 8,
-            textAlign: 'center',
-            width: '100%',
-            marginBottom: 10,
-            marginTop: 10,
-        },
-        txt:{
-            fontSize: RFPercentage(2.2),
-        },
+
+    container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#fcd6edff',
+    padding: 90,
+    },
+
+    paragraph: {
+    margin: 50,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#c9395dff',
+    },
+
+    entradaIMC:{
+    flex: 1,
+    flexDirection: 'row',
+    },
+
+    input:{
+    height: 80,
+    textAlign: 'center',
+    whidth: '50%',
+    fontSize: 50,
+    marginTop: 24,
+    color: '#862e62ff',
+    },
+
+    botao :{
+    backgroundColor: 'deeppink',
+    borderRadius: RFPercentage(2),
+    //padding: RFPercentage(1),
+    },
+
+    botaoText:{
+        alingSelf : 'center',
+        padding: 20,
+        fontSize: 25,
+        color: '#611341ff',
+        fontWeight: 'bold',
+    },
+
+    resultados:{
+        alingSelf: 'center',
+        color: 'lightgray',
+        fontSize: 65,
+        padding: 15,
+    },
+
     }
 );
 export default styles;
